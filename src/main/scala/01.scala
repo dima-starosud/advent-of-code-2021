da5640fba009a0d8206714477ea16a3763af6faa
Dmytro Starosud <d.starosud@gmail.com>
2021-12-02 11:00:32 +0200

    day_1

---
+++ src/main/scala/01.scala
@@ -0,0 +1,23 @@
+import scala.io.Source
+
+@main def hello: Unit =
+  val numbers = Source
+    .fromURL(getClass.getResource("/01/1.txt"))
+    .getLines()
+    .map(_.toInt)
+
+  val sums = numbers
+    .scanRight(Seq.empty[Int])(_ +: _ take 3)
+    .filter(_.size == 3)
+    .map(_.sum)
+
+  final case class State(previous: Int, count: Int)
+
+  val State(_, count) = sums.foldLeft(State(Int.MaxValue, 0))(
+    (state, value) =>
+      state.copy(
+        previous = value,
+        count = (if (value > state.previous) 1 else 0) + state.count)
+  )
+
+  println(count)
---
+++ src/test/scala/Test1.scala
@@ -0,0 +1,6 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class Test1:
+  @Test def t1(): Unit =
+    assertEquals("I was compiled by Scala 3. :)", "asdf")
