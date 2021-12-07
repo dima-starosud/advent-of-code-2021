8b4079c8dfb8d364dc29bfaa1707753d46219787
Dmytro Starosud <d.starosud@gmail.com>
2021-12-07 11:35:40 +0200

    small improvement

--- cpp/day_6/main.cpp
+++ cpp/day_6/main.cpp
@@ -9,20 +9,23 @@
 namespace rng = std::ranges;
 
 int main() {
-    std::array<long long int, 9> fish{};
+    std::array<uint64_t, 9> fish{};
     for (auto f1 : get_input()) {
         ++fish[f1];
     }
 
+    constexpr auto inc_mod_9 = [](auto& x) {
+        x = (x + 1) % 9;
+    };
+
+    size_t six = 6;
+    size_t eight = 8;
     for (int day : rng::views::iota(0, 256)) {
-        const auto zero_fish = fish[0];
-        for (size_t number = 1; number < fish.size(); ++number) {
-            fish[number - 1] = fish[number];
-        }
-        fish[6] += zero_fish;
-        fish[8] = zero_fish;
+        inc_mod_9(six);
+        inc_mod_9(eight);
+        fish[six] += fish[eight];
     }
 
-    const auto sum = std::accumulate(std::begin(fish), std::end(fish), static_cast<long long int>(0));
+    const auto sum = std::reduce(std::begin(fish), std::end(fish));
     std::cout << sum << std::endl;
 }
