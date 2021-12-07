93fed886e9d3b76c6b384921e446ecc2e1c17f8e
Dmytro Starosud <d.starosud@gmail.com>
2021-12-07 14:13:05 +0200

    day_7 c++ ranges style

--- cpp/day_7/main.cpp
+++ cpp/day_7/main.cpp
@@ -1,31 +1,28 @@
 #include "input_big.h"
 
-#include <limits>
-#include <array>
 #include <ranges>
+#include <execution>
+#include <algorithm>
+#include <numeric>
 
 #include <iostream>
 
-namespace rng = std::ranges;
+namespace ranges = std::ranges;
+namespace views = std::ranges::views;
 
 int main() {
     const auto crabs = get_input();
-    const auto[min_it, max_it] = std::minmax_element(std::begin(crabs), std::end(crabs));
-    const auto min_pos = *min_it;
-    const auto max_pos = *max_it;
 
-    Crab best_sum = std::numeric_limits<Crab>::max();
-
-    for (const Crab pos : rng::views::iota(min_pos, 1 + max_pos)) {
-        Crab sum = 0;
-        for (const Crab crab : crabs) {
-            const auto steps = std::abs(crab - pos);
-            sum += (1 + steps) * steps / 2;
-        }
-        if (sum < best_sum) {
-            best_sum = sum;
-        }
-    }
+    const auto best_sum = std::ranges::min(
+            views::iota(ranges::min(crabs), 1 + ranges::max(crabs))
+            | views::transform([&](const auto pos) {
+                const auto rng = (crabs | views::transform([&](const auto crab) {
+                    const auto steps = std::abs(crab - pos);
+                    return (1 + steps) * steps / 2;
+                }));
+                return std::reduce(std::begin(rng), std::end(rng));
+            })
+    );
 
     std::cout << best_sum << std::endl;
 }
